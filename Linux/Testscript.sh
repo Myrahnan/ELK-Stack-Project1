70800085c1dbf56332494abfb824bd59f096734a
Myrahnan <104738434+Myrahnan@users.noreply.github.com>
2022-05-02 19:46:22 -0400

    Create Testscript.sh

---
+++ Linux/Testscript.sh
@@ -0,0 +1,24 @@
+if [ ! -d /etc ]
+then
+echo " A "
+else " B "
+fi
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+"~/testscript.sh" 5L, 50C                                     1,1           All
