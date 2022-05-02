b9665b71f8d868d5c7084809637f555b207c3019
Myrahnan <104738434+Myrahnan@users.noreply.github.com>
2022-05-02 19:49:08 -0400

    Create Dealer.sh

---
+++ Linux/Dealer.sh
@@ -0,0 +1,23 @@
+#1/bin/bash
+cat $1_Dealer_schedule | awk -F" " '{print $1,$2,$5,$6}' | grep "$2:00:00 $3" >> Dealers_working_during_losses
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+<tion/Player_Dealer_Correlation/Dealer.sh" 2L, 123C           2,1           All
