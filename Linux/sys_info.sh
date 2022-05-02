276cba600c589099dbc122f3449dd0c1d0e19f4a
Myrahnan <104738434+Myrahnan@users.noreply.github.com>
2022-05-02 19:52:01 -0400

    Create sys_info.sh

---
+++ Linux/sys_info.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+output=$HOME/research/sys_info.txt
+ip=$(ip addr | head -9 | tail -1)
+execes=$(find /home -type f -perm 777)
+
+echo $ip
+echo -e "IP info:" >> $output
+echo -e "$ip \n" >> $output
+
+mkdir ~/research 2> /dev/null
+if [ ! -d $HOME/research ]
+then
+mkdir $HOME/research
+fi
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+"~/sys_info.sh" 15L, 279C                                     15,1          All
